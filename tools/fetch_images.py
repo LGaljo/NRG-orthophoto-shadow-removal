0623c352838a5273231a9ea2f556d857b7020dd9
Luka Galjot <luka.galjot@kalmia.si>
2023-05-19 16:35:44 +0200

    Rewrite fetch images script into class and add command line support

--- tools/fetch_images.py
+++ tools/fetch_images.py
@@ -1,5 +1,6 @@
 import os
 
+import argparse as argparse
 import requests
 from PIL import Image
 
@@ -17,82 +18,100 @@ The script takes the location of center tile and lod parameter.
 The default size is 20x20 tiles.
 """
 
-
 api_url = "http://gistiles1.arso.gov.si/nukleus_tiles2/Gis/NukleusTiles/v50/AgccTile.ashx"
-params = {
-    "r": 1402,
-    "c": 1334,
-    "lod": 16,
-    "lid": "lay_ao_dof_2019",
-    "f": "jpg"
-}
 
-tile_size = (20, 20)
-
-
-def get_map_tiles(center_r, center_c, lod):
-    params['lod'] = lod
-    start_r = center_r - int(tile_size[0] / 2)
-    end_r = center_r + int(tile_size[0] / 2)
-    start_c = center_c - int(tile_size[1] / 2)
-    end_c = center_c + int(tile_size[1] / 2)
-    for r in range(start_r, end_r):
-        for c in range(start_c, end_c):
-            params['r'] = r
-            params['c'] = c
-            get_map_tile()
+parser = argparse.ArgumentParser(description="Fetch images from Atlas Okolja",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--gcid', default="lay_AO_DOF_2019", help="GCID parameter")
+parser.add_argument('--lid', default="lay_ao_dof_2019", help="Layer id param")
+parser.add_argument('--lod', default=16, type=int, help="Level of details, number between [0, 16]")
+parser.add_argument('-f', default="jpg", help="Format of images (jpg or tiff)")
+parser.add_argument('-r', required=True, help="Location of G1 Generator model")
+parser.add_argument('-c', required=True, help="Location of G2 Generator model")
+parser.add_argument('-w', '--width', default=20, type=int, help="The number of tiles per axis")
+parser.add_argument('-o', '--output', default="../img", type=str, help="Output path")
 
 
-def get_map_tile():
-    imfn = './img/' + "%s_%s_%s_%s.%s" % (
-        params['lid'], str(params['r']), str(params['c']), str(params['lod']), params['f'])
+class AtlasOkoljaFetcher:
+    def __init__(self, args):
+        self.lid = args['lid']
+        self.lod = str(args['lod'])
+        self.gcid = str(args['gcid'])
+        self.size = (int(args['width']), int(args['width']))
+        self.r = int(args['r'])
+        self.c = int(args['c'])
+        self.f = str(args['f'])
+        self.output = str(args['output'])
+        self.s_r = int(self.r) - int(self.size[0] / 2)
+        self.e_r = int(self.r) + int(self.size[0] / 2)
+        self.s_c = int(self.c) - int(self.size[1] / 2)
+        self.e_c = int(self.c) + int(self.size[1] / 2)
 
-    if not os.path.exists('../img'):
-        os.mkdir('../img')
+        if not os.path.exists(self.output):
+            os.mkdir(self.output)
+            os.mkdir(os.path.join(self.output, "merged"))
 
-    if os.path.exists(imfn):
-        print("File already exists!")
-        return
-    else:
-        response = requests.get(api_url, params=params)
-        if response.status_code == 200:
-            # Open a new file in binary mode and write the image data to it
-            with open(imfn, "wb") as f:
-                f.write(response.content)
-            print("Image saved successfully!")
+    def fetch(self):
+        for r in range(self.s_r, self.e_r):
+            for c in range(self.s_c, self.e_c):
+                self.get_tile(r, c)
+
+    def get_tile(self, r, c):
+        tile_path = os.path.join(self.output, "%s_%s_%s_%s.%s" % (self.lid, str(r), str(c), self.lod, self.f))
+        params = {
+            "r": str(r),
+            "c": str(c),
+            "lod": str(self.lod),
+            "lid": str(self.lid),
+            "f": str(self.f),
+            "gcid": str(self.gcid),
+        }
+        if os.path.exists(tile_path):
+            print(f"Image {os.path.basename(tile_path)} already exists!")
+            return
         else:
-            print("Error saving image:", response.status_code)
+            response = requests.get(api_url, params=params)
+            if response.status_code == 200:
+                with open(tile_path, "wb") as f:
+                    f.write(response.content)
+                print(f"Image {os.path.basename(tile_path)} saved successfully!")
+            else:
+                print(f"Error saving image {os.path.basename(tile_path)}: {response.status_code}")
 
+    def merge(self):
+        # Check that all images exist
+        for r in range(self.s_r, self.e_r):
+            for c in range(self.s_c, self.e_c):
+                tile_path = os.path.join(self.output, "%s_%s_%s_%s.%s" % (self.lid, str(r), str(c), self.lod, self.f))
+                if not os.path.exists(tile_path):
+                    print(f"The tile r{r} c{c} does not exist")
+                    exit(1)
 
-def merge_image_tiles(center_r, center_c, ts=tile_size):
-    # Calculate the number of tiles needed to cover the image
-    start_r = int(center_r - ts[0] / 2)
-    end_r = int(center_r + ts[0] / 2)
-    start_c = int(center_c - ts[1] / 2)
-    end_c = int(center_c + ts[1] / 2)
+        # Create a new blank image with the same size as the original image
+        merged_image = Image.new("RGB", (self.size[0] * 256, self.size[1] * 256))
 
-    # Create a new blank image with the same size as the original image
-    merged_image = Image.new("RGB", (tile_size[0] * 256, tile_size[1] * 256))
+        # Loop through the tiles and paste them into the merged image
+        for r in range(self.s_r, self.e_r):
+            for c in range(self.s_c, self.e_c):
+                tile_path = os.path.join(self.output, "%s_%s_%s_%s.%s" % (self.lid, str(r), str(c), self.lod, self.f))
+                # Open the tile image
+                tile_image = Image.open(tile_path)
 
-    # Loop through the tiles and paste them into the merged image
-    for x in range(start_r, end_r):
-        for y in range(start_c, end_c):
-            # Open the tile image
-            tile_image = Image.open('./img/' + "%s_%s_%s_%s.%s" % (
-                params['lid'], str(x), str(y), str(params['lod']), params['f']))
+                # Calculate the position of the tile in the merged image
+                position = (c - self.c + self.size[0] / 2), (r - self.r + self.size[1] / 2)
+                paste_position = (int(position[0] * 256), int(position[1] * 256))
 
-            # Calculate the position of the tile in the merged image
-            position = (y - center_c + tile_size[0] / 2), (x - center_r + tile_size[1] / 2)
-            paste_position = (int(position[0] * 256), int(position[1] * 256))
+                # Paste the tile into the merged image
+                merged_image.paste(tile_image, paste_position)
 
-            # Paste the tile into the merged image
-            merged_image.paste(tile_image, paste_position)
-
-    path = f"{params['lid']}_{center_r}_{center_c}_{params['lod']}.jpg"
-    merged_image.save(path)
-    return path, merged_image
+        path = os.path.join(self.output, "merged", f"{self.lid}_{self.r}_{self.c}_{self.lod}.jpg")
+        merged_image.save(path)
+        return path, merged_image
 
 
 if __name__ == '__main__':
-    get_map_tiles(2000, 1500, 16)
-    merge_image_tiles(2000, 1500)
+    args = parser.parse_args()
+
+    fetcher = AtlasOkoljaFetcher(args.__dict__)
+    fetcher.fetch()
+    fetcher.merge()
