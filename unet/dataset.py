Fix model: set padding to reflect edge, add mean/std dataset normalization

unet/dataset.py
@@ -5,18 +5,20 @@ import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision.transforms.v2 import functional as F, Compose, ToImage, ToDtype
+from torchvision.transforms.v2 import functional as F, Compose, ToImage, ToDtype, Normalize
 from torchvision.transforms.v2 import ToTensor
 
 from unet import config
 
 
 class ImageLoaderDataset(Dataset):
-    def __init__(self, train_paths, gt_paths, transforms):
+    def __init__(self, train_paths, gt_paths, transforms, mean, std):
         # store the image and mask filepaths, and augmentation transforms
         self.train_paths = train_paths
         self.gt_paths = gt_paths
         self.transforms = transforms
+        self.mean = mean
+        self.std = std
 
     def __len__(self):
         # return the number of total samples contained in the dataset
@@ -32,8 +34,8 @@ class ImageLoaderDataset(Dataset):
 
         # Save a copy of transformed images
         if config.SAVE_TRANSFORMS:
-            image_train.save(os.path.basename(self.train_paths[idx]).split('.')[0] + "1.png")
-            image_gt.save(os.path.basename(self.gt_paths[idx]).split('.')[0] + "2.png")
+            image_train.save(os.path.basename(self.train_paths[idx]).split('.')[0] + " transform-train.png")
+            image_gt.save(os.path.basename(self.gt_paths[idx]).split('.')[0] + " transform-gt.png")
 
         # return a tuple of the image and its mask
         return image_train_tensor, image_gt_tensor
@@ -115,9 +117,17 @@ class ImageLoaderDataset(Dataset):
             image_gt = F.rotate(image_gt, angle=angle)
 
         # to_tensor = ToTensor()
+        # Create a transformation to convert images to tensors with automatic scaling [0,1]
         to_tensor = Compose([
             ToImage(),
-            ToDtype(torch.float32, scale=True)
+            ToDtype(torch.float32, scale=True),
+            Normalize(mean=self.mean, std=self.std)
         ])
 
-        return to_tensor(image_train), to_tensor(image_gt), image_train, image_gt
+        # Apply the transformation to both images
+        image_train_tensor = to_tensor(image_train)
+        image_gt_tensor = to_tensor(image_gt)
+
+
+
+        return image_train_tensor, image_gt_tensor, image_train, image_gt

unet/train.py
@@ -4,6 +4,7 @@
 import os
 
 from piqa import SSIM
+from PIL import Image
 
 from dataset import ImageLoaderDataset
 import config
@@ -30,9 +31,9 @@ class SSIMLoss(SSIM):
         return 1. - super().forward(x, y)
 
 
-class Loss(Module):
-    def __init__(self, alpha=0.5, ):
-        super(Loss, self).__init__()
+class SMLoss(Module):
+    def __init__(self, alpha=0.5):
+        super(SMLoss, self).__init__()
         self.alpha = alpha
         self.sp_loss = SPLoss()
         self.mse_loss = MSELoss()
@@ -75,7 +76,6 @@ def write_info():
     ])
     info_file.flush()
 
-
 def load_data():
     # load the image and mask filepaths in a sorted manner
     shadow_image = []
@@ -111,7 +111,7 @@ def load_data():
 
     train_transforms = [
         'RandomResizedCrop',
-        'ColorJitter',
+        # 'ColorJitter',
         'RandomHorizontalFlip'
         'RandomVerticalFlip',
         'RandomRotation',
@@ -119,7 +119,7 @@ def load_data():
 
     pretrain_transforms = transforms.Compose([
         'Resize',
-        'ColorJitter',
+        # 'ColorJitter',
         'GaussianNoise',
         'RandomHorizontalFlip'
         'RandomVerticalFlip',
@@ -127,8 +127,8 @@ def load_data():
     ])
 
     # create the train and evaluation datasets
-    trainDS = ImageLoaderDataset(train_paths=train_si, gt_paths=train_gti, transforms=train_transforms)
-    evalDS = ImageLoaderDataset(train_paths=eval_si, gt_paths=eval_gti, transforms=train_transforms)
+    trainDS = ImageLoaderDataset(train_paths=train_si, gt_paths=train_gti, transforms=train_transforms, mean=config.MEAN, std=config.STD)
+    evalDS = ImageLoaderDataset(train_paths=eval_si, gt_paths=eval_gti, transforms=train_transforms, mean=config.MEAN, std=config.STD)
     print(f"[INFO] found {len(trainDS)} examples in the training set...")
     print(f"[INFO] found {len(evalDS)} examples in the eval set...")
 
@@ -137,6 +137,8 @@ def load_data():
         f"Evaluation set transforms: {train_transforms}\n\n",
         f"Train set contains {len(trainDS)} image pairs\n\n",
         f"Evaluation set contains {len(evalDS)} image pairs\n\n",
+        f"Dataset mean: {config.MEAN}\n\n",
+        f"Dataset std: {config.STD}\n\n",
     ])
     info_file.flush()
 
@@ -179,7 +181,6 @@ def train(unet, trainLoader, evalLoader, trainSteps, evalSteps):
     # lossFunc = SSIMLoss().to(config.DEVICE)
     # lossFunc = SMLoss(0.5).to(config.DEVICE)
     # lossFunc = SPLoss().to(config.DEVICE)
-    # lossFunc = Loss(0.5).to(config.DEVICE)
     optimizer = AdamW(unet.parameters(), lr=config.INIT_LR, weight_decay=config.WEIGHT_DECAY)
 
     torch.backends.cudnn.benchmark = True
@@ -257,8 +258,11 @@ def train(unet, trainLoader, evalLoader, trainSteps, evalSteps):
 
     # display the total time needed to perform the training
     endTime = time.time()
+    print("[INFO] total time taken to train the model: {:.2f}s".format(endTime - startTime))
+    info_file.writelines(["[INFO] total time taken to train the model: {:.2f}s".format(endTime - startTime)])
+    info_file.flush()
 
-    print_results(startTime, endTime, H)
+    show_plot(H)
 
 
 def print_results(startTime, endTime, H):